b93a260c190a5232e27941ab7d8cf2b9645e9410
Md. Tanvir Ahmed <tanvirahmed.cse.bubt@gmail.com>
2019-11-20 20:22:27 +0600

    made flask

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 import result_with_input_pros2 as mdl
 import codecs
+from flask import Flask, jsonify
+app = Flask(__name__)
+
+@app.route('/<inp>')
+def index(inp):
+    #print(inp)
+    #f = codecs.open("input.txt", "r", "utf8")
+    #input = f.read()
+
+    mdl.getresult(inp)
+    return "hello world"
 
 
-f = codecs.open("input.txt", "r", "utf8")
-input = f.read()
-mdl.getresult(input)
+if __name__ == "__main__":
+    app.run(debug=True)
+
